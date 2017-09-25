6de68c2fc1de280caf92f29cefe7b5cc03d13238
Paul <paul.huynh3@gmail.com>
2017-09-25 17:47:40 -0400

    created the flickrapi and flickrphoto to handle the api

---
+++ Cats/Cats/FlickrPhoto.h
@@ -0,0 +1,26 @@
+//
+//  FlickrPhoto.h
+//  Cats
+//
+//  Created by Paul on 2017-09-25.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlickrPhoto : NSObject
+
+//create properties relating to the API
+@property (nonatomic,strong) NSString* flickrID;
+@property (nonatomic,strong) NSString* owner;
+@property (nonatomic,strong) NSString* secret;
+@property (nonatomic,strong) NSString* server;
+@property (nonatomic,strong) NSString* title;
+@property (nonatomic) NSInteger farm;
+
+
+- (instancetype)initWithInfo:(NSDictionary<NSString*,id>*)info;
+
+- (NSURL*) url;
+
+@end
